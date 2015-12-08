ddace528fed38bebc5539ab158a5e46ac72cac1d
AidanGG <AidanGG@users.noreply.github.com>
2015-12-08 12:34:06 +1100

    Added basic layer settings and a linear model.

--- ann/mlp.py
+++ ann/mlp.py
@@ -1,1 +1,15 @@
 import tensorflow as tf
+import numpy as np
+
+def hidden_layers(*layers):
+    weights = []
+    biases = []
+    for layer in range(len(layers) - 1):
+        weights.append(tf.Variable(tf.zeros([layers[layer] + 1, layers[layer]])))
+        biases.append(tf.Variable(tf.zeros([layers[layer] + 1, 1])))
+    return weights, biases
+
+def model(x, weights, biases):
+    for layer in range(len(weights)):
+        x = tf.matmul(weights[layer], x) + biases[layer]
+    return x
