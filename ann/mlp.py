d09169b93ef43bff1468c331d4e7f7c187e10b02
AidanGG <AidanGG@users.noreply.github.com>
2015-12-08 16:15:38 +1100

    Use transpose of vectors.

--- ann/mlp.py
+++ ann/mlp.py
@@ -7,13 +7,13 @@ def hidden_layers(*layers):
     biases = []
     for layer in range(len(layers) - 1):
         weights.append(
-            tf.Variable(tf.zeros([layers[layer + 1], layers[layer]])))
-        biases.append(tf.Variable(tf.zeros([layers[layer + 1], 1])))
+            tf.Variable(tf.zeros([layers[layer], layers[layer + 1]])))
+        biases.append(tf.Variable(tf.zeros([layers[layer + 1]])))
     return weights, biases
 
 
 def model(x, weights, biases):
-    y = tf.matmul(weights[0], x) + biases[0]
+    y = tf.nn.bias_add(tf.matmul(x, weights[0]), biases[0])
     for layer in range(1, len(weights)):
-        y = tf.matmul(weights[layer], y) + biases[layer]
+        y = tf.nn.bias_add(tf.matmul(y, weights[layer]), biases[layer])
     return y
