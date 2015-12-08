245b5ce1064f98b296543b776349439ff274ac66
AidanGG <AidanGG@users.noreply.github.com>
2015-12-08 15:26:42 +1100

    Fix up tensor dimensions.

--- ann/mlp.py
+++ ann/mlp.py
@@ -1,15 +1,19 @@
 import tensorflow as tf
 import numpy as np
 
+
 def hidden_layers(*layers):
     weights = []
     biases = []
     for layer in range(len(layers) - 1):
-        weights.append(tf.Variable(tf.zeros([layers[layer] + 1, layers[layer]])))
-        biases.append(tf.Variable(tf.zeros([layers[layer] + 1, 1])))
+        weights.append(
+            tf.Variable(tf.zeros([layers[layer + 1], layers[layer]])))
+        biases.append(tf.Variable(tf.zeros([layers[layer + 1], 1])))
     return weights, biases
 
+
 def model(x, weights, biases):
-    for layer in range(len(weights)):
-        x = tf.matmul(weights[layer], x) + biases[layer]
-    return x
+    y = tf.matmul(weights[0], x) + biases[0]
+    for layer in range(1, len(weights)):
+        y = tf.matmul(weights[layer], y) + biases[layer]
+    return y
