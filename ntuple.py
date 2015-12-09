aa1b34877a04ccd503b404208bef81fe511e6240
AidanGG <AidanGG@users.noreply.github.com>
2015-12-10 10:23:38 +1100

    Take transposes of tensors.

--- ntuple.py
+++ ntuple.py
@@ -6,14 +6,13 @@ import numpy as np
 def vector_to_one_hot(vector, classes):
     one_hot = []
 
-    for i in range(classes):
-        one_hot.append([])
-
     for i in range(len(vector)):
+        one_hot.append([])
         for j in range(classes):
-            one_hot[j].append(0)
+            one_hot[i].append(0)
             if vector[i] == j:
-                one_hot[j][i] = 1
+                one_hot[i][j] = 1
+
     return one_hot
 
 
@@ -21,21 +20,15 @@ def tntuple_to_arrays(ntuple, classifier_index, classes):
     x = []
     classifier = []
 
-    for column in range(ntuple.GetNvar() - 1):
-        x.append([])
-
     for row in range(ntuple.GetEntries()):
-        classifier_marker = True
+        x.append([])
         ntuple.GetEntry(row)
 
         for column in range(ntuple.GetNvar()):
             if column == classifier_index:
                 classifier.append(ntuple.GetArgs()[column])
-                classifier_marker = False
-            elif classifier_marker:
-                x[column].append(ntuple.GetArgs()[column])
             else:
-                x[column - 1].append(ntuple.GetArgs()[column])
+                x[row].append(ntuple.GetArgs()[column])
 
     y = vector_to_one_hot(classifier, classes)
 
