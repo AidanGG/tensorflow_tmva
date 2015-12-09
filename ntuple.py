ee083072de4d5b1877c6f5e8919159c9740910dc
AidanGG <AidanGG@users.noreply.github.com>
2015-12-09 14:36:40 +1100

    TNtuple now splits into two arrays.

--- ntuple.py
+++ ntuple.py
@@ -2,9 +2,24 @@ import ROOT
 import tensorflow as tf
 
 
-def tntuple_to_tensor(ntuple, classifier):
+def tntuple_to_tensor(ntuple, classifier_index):
+    data = []
+    classifier_data = []
+
+    for column in range(ntuple.GetNvar() - 1):
+        data.append([])
+
     for row in range(ntuple.GetEntries()):
+        classifier_marker = True
         ntuple.GetEntry(row)
 
         for column in range(ntuple.GetNvar()):
-            ntuple.GetArgs()[column]
+            if column == classifier_index:
+                classifier_data.append(ntuple.GetArgs()[column])
+                classifier_marker = False
+            elif classifier_marker:
+                data[column].append(ntuple.GetArgs()[column])
+            else:
+                data[column - 1].append(ntuple.GetArgs()[column])
+
+    return data, classifier_data
