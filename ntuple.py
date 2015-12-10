9e78f09bcb87fdb33330a66e00a511755eb1a8e2
AidanGG <AidanGG@users.noreply.github.com>
2015-12-10 13:09:26 +1100

    Allow for separate ntuples of different classes.

--- ntuple.py
+++ ntuple.py
@@ -33,3 +33,33 @@ def tntuple_to_arrays(ntuple, classifier_index, classes):
     y = vector_to_one_hot(classifier, classes)
 
     return x, y
+
+
+def tntuples_to_one_hot(tntuples):
+    one_hot = []
+
+    for i in range(len(tntuples)):
+        for j in range(tntuples[i].GetEntries()):
+            entry = []
+            for k in range(len(tntuples)):
+                if i == k:
+                    entry.append(1)
+                else:
+                    entry.append(0)
+            one_hot.append(entry)
+
+    return one_hot
+
+
+def tntuples_to_arrays(tntuples):
+    one_hot = tntuples_to_one_hot(tntuples)
+    x = []
+    for i in range(len(tntuples)):
+        for row in range(tntuples[i].GetEntries()):
+            tntuples[i].GetEntry(row)
+            entry = []
+            for column in range(tntuples[i].GetNvar()):
+                entry.append(tntuples[i].GetArgs()[column])
+            x.append(entry)
+
+    return x, one_hot
