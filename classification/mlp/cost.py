a52be12131f98e4e1e93ba8df713425eecd998e5
AidanGG <AidanGG@users.noreply.github.com>
2015-12-17 10:01:46 +1100

    Initial layout for cost functions.

---
+++ classification/mlp/cost.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+
+def mean_square_error(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
+
+
+def cross_entropy(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
+
+
+def linear(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
+
+
+def sigmoid(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
+
+
+def tanh(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
+
+
+def radial(model, data):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    return cost
