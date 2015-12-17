f81333b2b74705066e0b1b505d1b4fce7ffa928d
AidanGG <AidanGG@users.noreply.github.com>
2015-12-17 11:05:09 +1100

    Realised TMVA user guide is wrong.

--- classification/mlp/cost.py
+++ classification/mlp/cost.py
@@ -9,23 +9,3 @@ def mean_square_error(model, data):
 def cross_entropy(model, data):
     cost = -tf.reduce_sum(tf.mul(data, tf.log(model)))
     return cost
-
-
-def linear(model, data):
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-    return cost
-
-
-def sigmoid(model, data):
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-    return cost
-
-
-def tanh(model, data):
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-    return cost
-
-
-def radial(model, data):
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-    return cost
