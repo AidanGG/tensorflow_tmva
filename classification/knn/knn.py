725929b053b9a0e89ea5cc01706572b90e31a224
AidanGG <AidanGG@users.noreply.github.com>
2015-12-24 13:06:09 +1100

    Added model for knn.

--- classification/knn/knn.py
+++ classification/knn/knn.py
@@ -1,21 +1,49 @@
-from ..preprocessing import ttree
 import tensorflow as tf
 import numpy as np
 
 
-def knn(signal, background, testing, branches, nkNN=20, scale_frac=0.8,
-        trim=False, kernel="Gaus", use_kernel=False):
+def knn(training, one_hot, testing, nkNN=20, scale_frac=0.8, trim=False,
+        kernel="Gaus", use_kernel=False):
     return None
 
 
-def metric(training, test, scale_frac):
+def model(input_dims, output_dims, scale_frac, scales, nkNN):
+    training = tf.placeholder(tf.float32, shape=(None, input_dims))
+    one_hot = tf.placeholder(tf.float32, shape=(None, output_dims))
+    test = tf.placeholder(tf.float32, shape=(1, input_dims))
+    distances = metric(training, test, scale_frac, scales)
+
+    remaining_training = tf.identity(training)
+    remaining_one_hot = tf.identity(one_hot)
+
+    for i in range(nkNN):
+        min_slice = tf.to_int64(tf.concat(0, [tf.argmin(remaining_training, 0),
+                                              [-1]]))
+
+        start = tf.slice(remaining_training, tf.to_int64([0, 0]), min_slice)
+        end = tf.slice(remaining_training, min_slice + [1, 1], [-1, -1])
+        remaining_training = tf.concat(0, [start, end])
+
+        start = tf.slice(remaining_one_hot, tf.to_int64([0, 0]), min_slice)
+        end = tf.slice(remaining_one_hot, min_slice + [1, 1], [-1, -1])
+        class_slice = tf.slice(remaining_one_hot, min_slice + [0, 1], [1, -1])
+
+        if i == 0:
+            neighbour_one_hot = class_slice
+        else:
+            neighbour_one_hot = tf.concat(0, [neighbour_one_hot, class_slice])
+
+        one_hot = tf.concat(0, [start, end])
+    return tf.reduce_sum(neighbour_one_hot, reduction_indices=0)
+
+
+def metric(training, test, scale_frac, scales):
     if scale_frac == 0:
         distance = tf.sqrt(tf.reduce_sum(tf.square(tf.sub(training, test)),
                                          reduction_indices=1, keep_dims=True))
     else:
         distance = tf.sqrt(tf.reduce_sum(
-            tf.square(tf.div(tf.sub(training, test),
-                      scale(training, scale_frac))),
+            tf.square(tf.div(tf.sub(training, test), scales)),
             reduction_indices=1, keep_dims=True))
     return distance
 
