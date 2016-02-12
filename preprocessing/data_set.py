ab619a3f6d5cf6d5c2c7813f38366d8ce4f52de7
AidanGG <AidanGG@users.noreply.github.com>
2016-02-12 11:24:37 +1100

    Added batching.

--- preprocessing/data_set.py
+++ preprocessing/data_set.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 class DataSet(object):
+    """Based on the code provided in TensorFlow's MNIST example."""
 
     def __init__(self, data, one_hot):
         self._data = data
@@ -9,6 +10,8 @@ class DataSet(object):
         self._data_points = data.shape[0]
         self._dimensions = data.shape[1]
         self._classes = one_hot[1]
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
 
     def data(self):
         return self._data
@@ -24,3 +27,21 @@ class DataSet(object):
 
     def classes(self):
         return self._classes
+
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size):
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._data_points:
+            self._epochs_completed += 1
+            perm = np.arange(self._data_points)
+            np.random.shuffle(perm)
+            self._data = self._data[perm]
+            self._one_hot = self._one_hot[perm]
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._data_points
+        end = self._index_in_epoch
+        return self._data[start:end], self._one_hot[start:end]
