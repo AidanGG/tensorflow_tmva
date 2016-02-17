08fb5bfa399de82d4253e6f9a549ae4a2c1b3e11
AidanGG <AidanGG@users.noreply.github.com>
2016-02-17 14:33:41 +1100

    Small fixes.

--- mlp_test.py
+++ mlp_test.py
@@ -6,8 +6,10 @@ import tensorflow_tmva.preprocessing.data_set
 import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-my_mnist = tensorflow_tmva.preprocessing.data_set.DataSet(
+my_train = tensorflow_tmva.preprocessing.data_set.DataSet(
     mnist.train.images, mnist.train.labels, 10)
+my_test = tensorflow_tmva.preprocessing.data_set.DataSet(
+    mnist.test.images, mnist.test.labels, 10)
 
 x, y, W, b = tensorflow_tmva.classification.mlp.model.model(
     784, 10, [], neuron_type="linear", neuron_input_type="sum")
@@ -21,11 +23,10 @@ sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
-    batch_xs, batch_ys = my_mnist.next_batch(100)
+    batch_xs, batch_ys = my_train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-batch_xs, batch_ys = my_mnist.next_batch(10000)
-print(sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys}))
+print sess.run(accuracy, feed_dict={x: my_test.data, y_: my_test.labels})
