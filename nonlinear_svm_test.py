372325933b5328a548b1f20f6f261d3c9e2ae87b
AidanGG <AidanGG@users.noreply.github.com>
2016-02-18 14:53:25 +1100

    Fixed nonlinear svm test.

--- nonlinear_svm_test.py
+++ nonlinear_svm_test.py
@@ -6,7 +6,7 @@ import tensorflow_tmva.classification.svm.cost
 
 signal = ROOT.TNtuple("ntuple", "ntuple", "x:y")
 background = ROOT.TNtuple("ntuple", "ntuple", "x:y")
-for i in range(10000):
+for i in range(100):
     signal.Fill(ROOT.gRandom.Gaus(1, 1), ROOT.gRandom.Gaus(1, 1))
     background.Fill(ROOT.gRandom.Gaus(-1, 1), ROOT.gRandom.Gaus(-1, 1))
 
@@ -16,7 +16,7 @@ binary = tensorflow_tmva.preprocessing.ttree.ttrees_to_internal(
 input_tensor = tf.placeholder(tf.float32, [None, 2])
 labels_tensor = tf.placeholder(tf.float32, [None, 1])
 beta, offset, cost = tensorflow_tmva.classification.svm.cost.kernelised_cost(
-    input_tensor, labels_tensor, 20000, C=1, gamma=1.0)
+    input_tensor, labels_tensor, 200, C=1, gamma=1.0)
 
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
 
@@ -30,5 +30,5 @@ for i in range(1000):
 
 w = tensorflow_tmva.classification.svm.cost.beta_to_w(input_tensor, beta)
 
-print sess.run(w)
+print sess.run(w, feed_dict={input_tensor: binary.data()})
 print sess.run(offset)
