827e943e63a990fca8159a8eb120fd9cdfce5d8b
AidanGG <AidanGG@users.noreply.github.com>
2016-02-18 12:42:25 +1100

    Linear SVM test done.

--- linear_svm_test.py
+++ linear_svm_test.py
@@ -1,4 +1,5 @@
 import ROOT
+import tensorflow as tf
 import tensorflow_tmva.preprocessing.ttree
 import tensorflow_tmva.preprocessing.data_set
 import tensorflow_tmva.classification.svm.cost
@@ -11,3 +12,21 @@ for i in range(10000):
 
 binary = tensorflow_tmva.preprocessing.ttree.ttrees_to_internal(
     [signal, background], ["x", "y"], binary=True)
+
+input_tensor = tf.placeholder(tf.float32, [None, 2])
+labels_tensor = tf.placeholder(tf.float32, [None, 1])
+w, offset, cost = tensorflow_tmva.classification.svm.cost.unkernelised_cost(
+    input_tensor, labels_tensor, 2, C=10)
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={
+             input_tensor: binary.data(), labels_tensor: binary.labels()})
+
+print sess.run(w)
+print sess.run(offset)
