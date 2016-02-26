159cdecef2463a1a1c2cb04787bb203716cd5389
AidanGG <AidanGG@users.noreply.github.com>
2016-02-26 13:00:56 +1100

    Comment IO test.

--- examples/root_file_read_test.py
+++ examples/root_file_read_test.py
@@ -7,23 +7,24 @@ branches = ["k0mm2", "k0et", "k0hso00", "k0hso01", "k0hso02", "k0hso03",
             "k0hso24", "k0hoo0", "k0hoo1", "k0hoo2", "k0hoo3", "k0hoo4",
             "cos_thr", "deltaz", "cosb"]
 
-signal_file = ROOT.TFile("signal.root")
-continuum_file = ROOT.TFile("continuum.root")
+# Gets TTrees from ROOT files.
+signal_file = ROOT.TFile("ROOT_data/signal.root")
+continuum_file = ROOT.TFile("ROOT_data/continuum.root")
 signal_tree = signal_file.Get("h101;1")
 continuum_tree = continuum_file.Get("h101;1")
 
+# Stores TTrees in our data structure.
 multiclass = preprocessing.ttree.ttrees_to_internal(
     [signal_tree, continuum_tree], branches, binary=False)
-
 binary = preprocessing.ttree.ttrees_to_internal(
     [signal_tree, continuum_tree], branches, binary=True)
 
+# Prints data from data structure.
 print(multiclass.data())
 print(multiclass.labels())
 print(multiclass.data_points())
 print(multiclass.dimensions())
 print(multiclass.classes())
-
 print(binary.data())
 print(binary.labels())
 print(binary.data_points())
