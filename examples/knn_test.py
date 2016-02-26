5e3626bb162e3f1cfdffc0bb7d7bbf61cb215c31
AidanGG <AidanGG@users.noreply.github.com>
2016-02-26 12:44:54 +1100

    Commented knn example.

--- examples/knn_test.py
+++ examples/knn_test.py
@@ -6,6 +6,7 @@ import preprocessing.data_set
 import preprocessing.ttree
 import ROOT
 
+# Generates two 3D normal distributions in two TTrees.
 signal = ROOT.TNtuple("ntuple", "ntuple", "x:y:z")
 background = ROOT.TNtuple("ntuple", "ntuple", "x:y:z")
 for i in range(10000):
@@ -14,21 +15,26 @@ for i in range(10000):
     background.Fill(ROOT.gRandom.Gaus(-1, 20),
                     ROOT.gRandom.Gaus(-1, 20), ROOT.gRandom.Gaus(-1, 20))
 
-
+# Reads the TTrees into our data structure.
 data = preprocessing.ttree.ttrees_to_internal(
     [signal, background], ["x", "y", "z"], binary=False)
 
+# Sets up a test instance.
 test = np.array([[0., 0., 0.]])
 
-scales = knn.scale(data.data(), 0.9)
+# Gets the scales to use for the distribution widths
+scale_frac = 0.8
+scales = knn.scale(data.data(), scale_frac)
 
+# Gets relevent tensors from k-NN model.
 train_tensor, labels_tensor, test_tensor, neighbours = knn.model_single(
-    3, 2, 0.9, scales, 100)
+    3, 2, scale_frac, scales, 100)
 
 sess = tf.Session()
 
 print("Searching for 100 nearest neighbours")
 
+# Feeds all data into model.
 results = sess.run(neighbours,
                    feed_dict={train_tensor: data.data(),
                               labels_tensor: data.labels(), test_tensor: test})
